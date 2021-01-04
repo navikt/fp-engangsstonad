8b4f65ab9eff4c5296f723c8784301bfafdbfe1f
Andreas Dybdahl <andreas.dybdahl@nav.no>
2021-01-04 12:02:47 +0100

    Dratt inn velkommen banner fra felleskomponentbibliotek

--- src/app/velkommen/Velkommen.tsx
+++ src/app/velkommen/Velkommen.tsx
@@ -1,13 +1,11 @@
 import React, { FunctionComponent } from 'react';
-import SimpleIllustration from 'components/simple-illustration/SimpleIllustration';
 import { FormattedMessage, useIntl } from 'react-intl';
-import intlHelper from '@navikt/fp-common/lib/utils/intlUtils';
 import { Ingress, Innholdstittel } from 'nav-frontend-typografi';
-import { bemUtils, LanguageToggle } from '@navikt/fp-common';
+import { bemUtils, LanguageToggle, VelkommenBanner, intlUtils } from '@navikt/fp-common';
+import Veiviser from 'components/veiviser/VeiviserSvg';
+import Veilederpanel from 'nav-frontend-veilederpanel';
 
 import './velkommen.less';
-import Veilederpanel from 'nav-frontend-veilederpanel';
-import Veiviser from 'components/veiviser/VeiviserSvg';
 
 interface Props {
     fornavn: string;
@@ -20,17 +18,17 @@ const Velkommen: FunctionComponent<Props> = ({ fornavn }) => {
     return (
         <>
             <LanguageToggle locale="nb" availableLocales={['en', 'nb', 'nn']} toggle={() => null} />
-            <SimpleIllustration
+            <VelkommenBanner
                 dialog={{
-                    text: intlHelper(intl, 'intro.standard.bobletekst'),
-                    title: intlHelper(intl, 'intro.standard.bobletittel', { name: fornavn }),
+                    text: intlUtils(intl, 'intro.standard.bobletekst'),
+                    title: intlUtils(intl, 'intro.standard.bobletittel', { name: fornavn }),
                 }}
             />
             <div className={bem.block}>
                 <div className={bem.element('tittel')}>
-                    <Innholdstittel>{intlHelper(intl, 'intro.standard.velkommentittel')}</Innholdstittel>
+                    <Innholdstittel>{intlUtils(intl, 'intro.standard.velkommentittel')}</Innholdstittel>
                 </div>
-                <Ingress>{intlHelper(intl, 'intro.standard.ingress')}</Ingress>
+                <Ingress>{intlUtils(intl, 'intro.standard.ingress')}</Ingress>
                 <Veilederpanel kompakt={true} svg={<Veiviser />}>
                     <FormattedMessage id="intro.text.veiviser" />
                     <br />
