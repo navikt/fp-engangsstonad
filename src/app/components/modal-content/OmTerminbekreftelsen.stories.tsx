acdc1eb67fb338259d9e82ee4d0fb100b720f543
tor-nav <tor.roneid@nav.no>
2023-03-06 11:33:32 +0100

    Skift ut enzyme med testing-library

--- src/app/components/modal-content/OmTerminbekreftelsen.stories.tsx
+++ src/app/components/modal-content/OmTerminbekreftelsen.stories.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Story } from '@storybook/react'; // eslint-disable-line import/no-extraneous-dependencies
 import IntlProvider from 'intl/IntlProvider';
 
-
 import OmTerminbekreftelsen from './OmTerminbekreftelsen';
-
 
 export default {
   title: 'OmTerminbekreftelsen',
   component: OmTerminbekreftelsen,
 };
 
-const Template: Story<any> = () => (
+const Template: Story = () => (
   <IntlProvider språkkode="nb">
     <OmTerminbekreftelsen />
   </IntlProvider>
